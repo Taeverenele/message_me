036a806d88c1ab0f7219fd2414b85c6693327cf0
nele <ntaevere@gmail.com>
2020-07-17 14:18:51 +1000

    display messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Nele", password: "passwordnele")
+User.create(username: "Mike", password: "passwordmike")
+User.create(username: "Jane", password: "passwordjane")
+User.create(username: "Billy", password: "passwordbilly")
+User.create(username: "Mary", password: "passwordmary")
